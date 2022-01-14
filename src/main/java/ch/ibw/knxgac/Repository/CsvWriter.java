3fc8ec7e7606c3ab3516f87f0dcc1d8746038bb2
Urs Heusser <urs.heusser@fhgr.ch>
2022-01-14 09:45:34 +0100

    CSV Export sort

--- src/main/java/ch/ibw/knxgac/Repository/CsvWriter.java
+++ src/main/java/ch/ibw/knxgac/Repository/CsvWriter.java
@@ -9,6 +9,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class CsvWriter {
     private String filePath;
@@ -20,22 +21,29 @@ public class CsvWriter {
     }
 
     public boolean writeProjectToCsv(Project project) throws IOException {
+        // Templates for repeating strings
         String templatemag1 = " ; ;";
         String templatemag2 = "\"\";\"\";\"\";\"\";\"AUTO\"";
         String templatemig = "\"\";\"\";\"\";\"AUTO\"";
         String templateadr = "\"\";\"\";\"AUTO\"";
+
+        // create a string-array with the csv-export lines
         ArrayList<String> export = new ArrayList<>();
         for(MainGroup mainGroup : project.getMaingroups()) {
              export.add("\"" + mainGroup.getName() + "\";" + templatemag1 + "\"" + mainGroup.getNumber() + "\";" + templatemag2);
             for(MiddleGroup middleGroup : mainGroup.getMiddlegroups()) {
                 export.add(" ;\"" + middleGroup.getName() + "\";" + " ;" + "\"" + mainGroup.getNumber() + "\";" + "\"" + middleGroup.getNumber() + "\";" + templatemig);
+                Collections.sort(middleGroup.getAddresses());
                 for (Address address: middleGroup.getAddresses()) {
+                    Collections.sort(address.getObjectTemplate().getAttributes());
                     for (Attribute attribute : address.getObjectTemplate().getAttributes()) {
                         export.add(" ; ;\"" + address.getName() + " " + attribute.getName() + "\";\"" + mainGroup.getNumber() + "\";\"" + middleGroup.getNumber() + "\";\"" + (address.getStartAddress() + attribute.getNumber()) + "\";" + templateadr);
                     }
                 }
             }
         }
+
+        // generate the csv-string to write to file
         String csv = "";
         for (String s : export) {
             csv = csv + s + "\n";
