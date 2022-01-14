6295783191cdd7112dfdbc0843e0585a93d04550
Urs Heusser <urs.heusser@fhgr.ch>
2022-01-14 17:07:06 +0100

    availableStartadresses implemented

--- src/main/java/ch/ibw/knxgac/Model/ObjectTemplate.java
+++ src/main/java/ch/ibw/knxgac/Model/ObjectTemplate.java
@@ -1,6 +1,8 @@
 package ch.ibw.knxgac.Model;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class ObjectTemplate extends Data {
@@ -57,23 +59,69 @@ public class ObjectTemplate extends Data {
         return result.toString();
     }
 
-    public int[] availableStartadresses(int[] usedStartadresses, int[]maxStartadresses) {
+    public ArrayList<Integer> availableStartadresses(ArrayList<Integer> maxStartadresses, ArrayList<Integer> usedStartadresses) {
         // berechne den unterschied von maxStartadresse und usedStartadresse
-        int[] uniqueAdresses = IntStream.concat(IntStream.of(maxStartadresses), IntStream.of(usedStartadresses))
-                .filter(x -> !IntStream.of(maxStartadresses).anyMatch(y -> y == x) || !IntStream.of(usedStartadresses).anyMatch(z -> z == x))
-                .toArray();
+        int[] uniqueAdresses = diff(this.arrayListToArray(maxStartadresses), this.arrayListToArray(usedStartadresses));
 
-        Integer anzAdr = this.getAttributes().size();
-        for (int i = 0; i < anzAdr; i++) {
-            // pruefe ob die aktelle und x-weitere verfuegbar sind
-
+        ArrayList<Integer> digitsToCheck = new ArrayList<>();
+        ArrayList<Integer> toDelete = new ArrayList<>();
+        for(int checkAdr : uniqueAdresses) {
+            Integer anzAdr = this.getAttributes().size();
+            int[] toCheck = new int[anzAdr];
+            for (int i = 0; i < anzAdr; i++) {
+                // pruefe ob die aktelle und x-weitere verfuegbar sind
+                digitsToCheck.add(checkAdr+i);
+                toCheck[i] = checkAdr+i;
+            }
+            // sind die toCheck im uniqueAdresses -> dann ok
+            int[] uebereinstimmung = this.intersection(uniqueAdresses, toCheck);
+            if(uebereinstimmung.length!=anzAdr) {
+                toDelete.add(checkAdr);
+            }
         }
-        return uniqueAdresses;
+        // von arraylist zu array
+        int[] toDeleteArray = new int[toDelete.size()];
+        for (int i = 0; i < toDelete.size(); i++) {
+            toDeleteArray[i] = toDelete.get(i);
+        }
+        int[] availableArray = this.diff(uniqueAdresses, toDeleteArray);
+        ArrayList<Integer> available = new ArrayList<>();
+        for (int x : availableArray) {
+            available.add(x);
+        }
+        return available;
     }
-//    public  int[] intersectionSimple(int[] a, int[] b){
-//        IntStream.of(a).filter(Arrays.asList(IntStream.of(b)):con)
-//        return IntStream.of(a).filter(Arrays.asList(b)::contains);
-//    }
+
+    /**
+     * wandelt eine int-arraylist in ein array um
+     */
+    private int[] arrayListToArray(ArrayList<Integer> arrayList) {
+        int[] array = new int[arrayList.size()];
+        for (int i = 0; i < arrayList.size(); i++) {
+            array[i] = arrayList.get(i);
+        }
+        return array;
+    }
+    /**
+     * holt die differenz zweier int-arrays
+     */
+    private int[] diff(int[] a, int[] b){
+        // Todo https://stackoverflow.com/questions/17863319/java-find-intersection-of-two-arrays
+        return IntStream.concat(IntStream.of(a), IntStream.of(b))
+                .filter(x -> !IntStream.of(a).anyMatch(y -> y == x) || !IntStream.of(b).anyMatch(z -> z == x))
+                .toArray();
+    }
+
+    /**
+     * holt die übereinstimmenden werte aus zwei int-arrays
+     */
+    private int[] intersection(int[] a, int[] b){
+        // Todo https://stackoverflow.com/questions/17863319/java-find-intersection-of-two-arrays
+        return Arrays.stream(a)
+                .distinct()
+                .filter(x -> Arrays.stream(b).anyMatch(y -> y == x))
+                .toArray();
+    }
 
 
 
